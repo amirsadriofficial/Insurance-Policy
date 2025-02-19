0fcca4a423842458db4bcfc65df49f0b3f116e11
Amir Sadri <amirsadriofficial@gmail.com>
2025-02-19 15:37:27 +0330

    [FEAT]: add car plate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CarPlate from "@/components/car-plate";
 import TitleBar from "@/components/title-bar";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div>
       <TitleBar title="مشخصات بیمه نامه" />
       <TitleBar title="مشخصات مالک خودرو" />
+      <CarPlate />
     </div>
   );
 }
--- src/components/car-plate/index.tsx
+++ src/components/car-plate/index.tsx
@@ -1,1 +1,29 @@
- 
+import React from "react";
+import IranFlag from "@/assets/icons/iran-flag.svg";
+
+// interface IProps {
+//   carNumber?: string;
+// }
+
+function CarPlate() {
+  return (
+    <div className="flex h-[50px] w-max rounded-[5px] border-2 border-[#000]">
+      <p className="my-auto mx-[16px] font-semibold">60</p>
+      <div className="inline-block h-full w-[2px] self-stretch bg-[#000]"></div>
+      <div className="my-auto mx-[16px] flex gap-3 font-semibold">
+        <p>988</p>
+        <p>ک </p>
+        <p>64</p>
+      </div>
+      <div className="inline-block h-full w-[2px] self-stretch bg-[#000]"></div>
+      <div className="bg-[#1D48E1] pt-[6px] px-[9px]">
+        <IranFlag />
+        <p className="text-[#fff] font-semibold" dir="ltr">
+          I.R.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CarPlate;
