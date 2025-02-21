e4246aad391a8dad0c1b0cbdbece122aab7f6e29
Amir Sadri <amirsadriofficial@gmail.com>
2025-02-21 14:38:29 +0330

    [FEAT]: apply dana font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,32 @@
 import { Metadata } from "next";
 import WithQueryClientProvider from "@/hoc/withQueryClientProvider";
+import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Bime Bazar",
   description: "Insurance policy",
 };
+
+const danaFont = localFont({
+  src: [
+    {
+      path: "../../public/dana-fanum-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/dana-fanum-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/dana-fanum-demibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
-      <body>
+      <body className={danaFont.className}>
         <WithQueryClientProvider wrappedComponent={children} />
       </body>
     </html>
